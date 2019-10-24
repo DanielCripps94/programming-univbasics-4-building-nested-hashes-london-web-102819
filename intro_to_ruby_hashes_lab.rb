05f99d06941384dcbb021841cdb7000f52fa9b60
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-24 15:51:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ def monopoly_with_third_tier
   monopoly = {
 		railroads: {
 			pieces: 4,
-			names: {kings_cross:{},marlyebone: {},frenchurch_street:{},liverpool_street:{}},
-			rent_in_dollars: {one_piece_owned: 20, two_pieces_owned: 40, three_pieces_owned: 60, four_pieces_owned:80}
+			names: {reading_railroad:{},pennsylvania_railroad: {},b_and_o_railroad:{},shortline_railroad:{}},
+			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 		}
 	}
 end
